44178efc9c3ac65bc93cb7b20cfc198c93a467be
frmlp <formelapiotr@wp.pl>
2024-11-20 20:42:55 +0100

    new comments

--- routes/admin.php
+++ routes/admin.php
@@ -4,26 +4,24 @@ use App\Http\Controllers\Admin\PointController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::prefix('admin')->group(function() { // utworzenie grupy endpointów z prefixem 'api/admin/'
+Route::prefix('admin')->group(function() { // utworzenie grupy endpointów z prefixem '/admin/'
     
 
     Route::middleware('auth')->group(function () { // endpointy w tej grupie wymagają żeby użytkownik był zalogowany
 
-        // endpoint 'api/admin/zpk'
+        // endpoint '/admin/zpk'
         Route::get('/zpk', function() {
             return view('admin.zpk');
         })->name('admin.zpk');
-        
-        //endpoint 'api/
 
         // definicja endpointów RESTfull dla zasobu points
         // utworzenie endpointów:
-        // POST api/admin/points
-        // PUT|Patch api/admin/points/{point}
-        // DELETE api/admin/points/{point}
+        // POST /admin/points
+        // PUT|Patch /admin/points/{point}
+        // DELETE /admin/points/{point}
         Route::apiResource('points', PointController::class) 
             ->scoped()->except(['index', 'show']);
-            // przyda się implementacja GET api/admin/docelowa-nazwa-zasobu
+            // przyda się implementacja GET /admin/docelowa-nazwa-zasobu
             // narazie frontend korzysta z endpointu zdefiniowanego w pliku 'api.php' co nie jest najlepszym rozwiązaniem
 
         
--- routes/api.php
+++ routes/api.php
@@ -13,11 +13,12 @@ use App\Models\Point;
 
 // w tym pliku znajdują się endpointy API, przez które pobiera się dane na frondend/mobile
 
+// ten endpoint jest generowany automatycznie, chyba niepotrzebny
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-// endpoint przez który pobiera się dane o trasach (BAZA TRAS)
+// endpoint przez który pobiera się dane o trasach (BAZA TRAS) 'api/paths'
 Route::get('/paths', function() {
     return PathResource::collection(Path::all());
 });
@@ -27,11 +28,13 @@ Route::get('/points', function() {
     return GeneratorPointResource::collection(Point::all());
 });
 
-// endpoint przez który pobiera się czystą mapę podkładową
+// endpoint przez który pobiera się czystą mapę podkładową 'api/map-download'
 Route::get('/map-download', MapDownloadController::class);
 
 // endpoint przez który wysyła się zapytanie z parametrami do generatora
 // jako odpowiedź zwrotna dostaje się wygenerowane trasy
 Route::get('/generator', GeneratorService::class);
 
-// implementacja endpointu, który na podstawieprzesłanego kodu zapisuje trasę w bazie danych
+
+// implementacja endpointu, który na podstawie przesłanego kodu zapisuje trasę w bazie danych
+
