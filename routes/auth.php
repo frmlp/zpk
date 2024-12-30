dc544f9db058e31714a82b4b8a899a1c7859a4f3
Andrzej Wroblewski <wroblewski.and@gmail.com>
2024-12-30 12:27:08 +0100

    dodawanie nowych użytkowników, niezabezpieczone

--- routes/auth.php
+++ routes/auth.php
@@ -13,6 +13,20 @@ Route::group(['middleware' => 'guest'], function() { // endpointy dla niezalogow
     
     Route::get('/login', [AuthController2::class, 'login'])->name('login'); // przekierowanie na stronę logowania
     Route::post('/login', [AuthController2::class, 'loginPost'])->name('login'); // wysłanie podanych danych logowania na serwer w celu autoryzacji
+    // ggh todo: zmiana nazewnictwa name()
+
+    // rejestracja nowych użytkowników
+    Route::get('/register', [AuthController2::class, 'register'])->name('register');
+    Route::post('/register', [AuthController2::class, 'registerPost'])->name('register.post');
+
+    // edycja istniejących użytkowników
+    Route::get('/profile', [AuthController2::class, 'profile'])->name('profile');
+    Route::put('/profile', [AuthController2::class, 'profileUpdate'])->name('profile.update');
+
+    // zmiana hasła
+    Route::get('/password/change', [AuthController2::class, 'changePassword'])->name('password.change');
+    Route::put('/password/change', [AuthController2::class, 'updatePassword'])->name('password.update');
+
 });
 
 Route::group(['middleware' => 'auth'], function () { // endpointy dla zalogowanych użytkowników
