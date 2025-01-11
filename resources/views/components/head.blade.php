7d33c13ee9b3cdb2651dfe2bc8922a4cc9b5a38e
frmlp <formelapiotr@wp.pl>
2025-01-11 21:13:42 +0100

    csrf token bug fix

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -1,5 +1,6 @@
 <head>
     <meta charset="UTF-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="target-densitydpi=device-dpi, width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, minimal-ui" />
     <title>@yield('title', 'ZPK')</title>
     <!-- BOOTSTRAP -->
