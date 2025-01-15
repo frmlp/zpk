c9fee67d0eb2914ebee7dd36772632ec5264973a
frmlp <formelapiotr@wp.pl>
2025-01-16 00:11:21 +0100

    walidacja krótkiej ścieżki

--- resources/views/pages/client/planer.blade.php
+++ resources/views/pages/client/planer.blade.php
@@ -6,6 +6,7 @@
     <div class="row  border border-success border-4 rounded bg-light">
         <div class="col-12 col-lg-6">
             <div class="m-3">
+                <div id="alertMessage" class="alert alert-danger mb-3"></div>
                 <div  id="dropdown-container" class="mb-3">
                     
                 </div>
